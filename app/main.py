e9e5a8f27b240b3574a7aaf8439cd49569818635
Yuto299 <ota.se.ke@outlook.jp>
2025-12-07 22:11:11 +0900

    TechTrainに書き換え

--- app/main.py
+++ app/main.py
@@ -30,24 +30,34 @@ class NewTodoItem(BaseModel):
     """TODO項目新規作成時のスキーマ."""
 
     title: str = Field(title="Todo Item Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo Item Description", min_length=1, max_length=200)
+    description: str | None = Field(
+        default=None, title="Todo Item Description", min_length=1, max_length=200
+    )
     due_at: datetime | None = Field(default=None, title="Todo Item Due")
 
 
 class UpdateTodoItem(BaseModel):
     """TODO項目更新時のスキーマ."""
 
-    title: str | None = Field(default=None, title="Todo Item Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo Item Description", min_length=1, max_length=200)
+    title: str | None = Field(
+        default=None, title="Todo Item Title", min_length=1, max_length=100
+    )
+    description: str | None = Field(
+        default=None, title="Todo Item Description", min_length=1, max_length=200
+    )
     due_at: datetime | None = Field(default=None, title="Todo Item Due")
-    complete: bool | None = Field(default=None, title="Set Todo Item status as completed")
+    complete: bool | None = Field(
+        default=None, title="Set Todo Item status as completed"
+    )
 
 
 class ResponseTodoItem(BaseModel):
     id: int
     todo_list_id: int
     title: str = Field(title="Todo Item Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo Item Description", min_length=1, max_length=200)
+    description: str | None = Field(
+        default=None, title="Todo Item Description", min_length=1, max_length=200
+    )
     status_code: TodoItemStatusCode = Field(title="Todo Status Code")
     due_at: datetime | None = Field(default=None, title="Todo Item Due")
     created_at: datetime = Field(title="datetime that the item was created")
@@ -58,14 +68,20 @@ class NewTodoList(BaseModel):
     """TODOリスト新規作成時のスキーマ."""
 
     title: str = Field(title="Todo List Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo List Description", min_length=1, max_length=200)
+    description: str | None = Field(
+        default=None, title="Todo List Description", min_length=1, max_length=200
+    )
 
 
 class UpdateTodoList(BaseModel):
     """TODOリスト更新時のスキーマ."""
 
-    title: str | None = Field(default=None, title="Todo List Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo List Description", min_length=1, max_length=200)
+    title: str | None = Field(
+        default=None, title="Todo List Title", min_length=1, max_length=100
+    )
+    description: str | None = Field(
+        default=None, title="Todo List Description", min_length=1, max_length=200
+    )
 
 
 class ResponseTodoList(BaseModel):
@@ -73,11 +89,14 @@ class ResponseTodoList(BaseModel):
 
     id: int
     title: str = Field(title="Todo List Title", min_length=1, max_length=100)
-    description: str | None = Field(default=None, title="Todo List Description", min_length=1, max_length=200)
+    description: str | None = Field(
+        default=None, title="Todo List Description", min_length=1, max_length=200
+    )
     created_at: datetime = Field(title="datetime that the item was created")
     updated_at: datetime = Field(title="datetime that the item was updated")
 
 
+# tagsはSwaggerUIでのグルーピングに使用します。
 @app.get("/hello", tags=["Hello"])
 def get_hello():
-    return {"Message": "Hello FastAPI!"}
+    return {"Message": "Hello TechTrain!"}
