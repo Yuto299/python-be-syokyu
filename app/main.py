913e5c43b4ce51c22082922c76c974f78dfd635f
Yuto299 <ota.se.ke@outlook.jp>
2025-12-10 22:35:08 +0900

    GET todo項目 APIを実装

--- app/main.py
+++ app/main.py
@@ -175,3 +175,25 @@ def delete_todo_list(
     db.commit()
 
     return {}
+
+
+# GET Todo項目
+@app.get(
+    "/lists/{todo_list_id}/items/{todo_item_id}",
+    response_model=ResponseTodoItem,
+    tags=["Todo項目"],
+)
+def get_todo_item(
+    todo_list_id: int,
+    todo_item_id: int,
+    db: Session = Depends(get_db),
+):
+    todo_item = (
+        db.query(ItemModel)
+        .filter(ItemModel.id == todo_item_id, ItemModel.todo_list_id == todo_list_id)
+        .first()
+    )
+    if not todo_item:
+        raise HTTPException(status_code=404, detail="Todo Item not found")
+
+    return todo_item
