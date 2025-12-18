c29a716e826fa5e016a85749cf2f2e8c0dc50584
Yuto299 <ota.se.ke@outlook.jp>
2025-12-18 21:21:30 +0900

    listの分割は完了

---
+++ app/crud/list_crud.py
@@ -0,0 +1,60 @@
+from sqlalchemy.orm import Session
+
+from app.models.list_model import ListModel
+
+
+# GET Todoリスト
+def get_todo_list(
+    todo_list_id: int,
+    db: Session,
+):
+    return db.query(ListModel).filter(ListModel.id == todo_list_id).first()
+
+
+# POST Todoリスト
+def post_todo_list(
+    title: str,
+    description: str | None,
+    db: Session,
+):
+    new_list = ListModel(
+        title=title,
+        description=description,
+    )
+    db.add(new_list)
+    db.commit()
+    db.refresh(new_list)
+
+    return new_list
+
+
+# PUT Todoリスト
+def put_todo_list(
+    todo_list_id: int,
+    title: str,
+    description: str | None,
+    db: Session,
+):
+    existing_list = db.query(ListModel).filter(ListModel.id == todo_list_id).first()
+
+    existing_list.title = title
+    if description is not None:
+        existing_list.description = description
+
+    db.commit()
+    db.refresh(existing_list)
+
+    return existing_list
+
+
+# DELETE Todoリスト
+def delete_todo_list(
+    todo_list_id: int,
+    db: Session,
+):
+    existing_list = db.query(ListModel).filter(ListModel.id == todo_list_id).first()
+
+    db.delete(existing_list)
+    db.commit()
+
+    return {}
