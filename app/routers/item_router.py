d6e3f3ae5e64434e012ae499e7e6fd757312f6ea
Yuto299 <ota.se.ke@outlook.jp>
2025-12-18 23:33:30 +0900

    Routerの設定完了

---
+++ app/routers/item_router.py
@@ -0,0 +1,68 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.crud import item_crud
+from app.dependencies import get_db
+from app.schemas.item_schema import NewTodoItem, ResponseTodoItem, UpdateTodoItem
+
+router = APIRouter(prefix="/lists/{todo_list_id}/items", tags=["Todo項目"])
+
+
+@router.get("/{todo_item_id}", response_model=ResponseTodoItem)
+def get_todo_item(
+    todo_list_id: int,
+    todo_item_id: int,
+    db: Session = Depends(get_db),
+):
+    todo_item = item_crud.get_todo_item(db, todo_list_id, todo_item_id)
+    if not todo_item:
+        raise HTTPException(status_code=404, detail="Todo Item not found")
+    return todo_item
+
+
+@router.post("", response_model=ResponseTodoItem, status_code=201)
+def post_todo_item(
+    todo_list_id: int,
+    todo_item: NewTodoItem,
+    db: Session = Depends(get_db),
+):
+    return item_crud.post_todo_item(
+        db,
+        todo_list_id,
+        todo_item.title,
+        todo_item.description,
+        todo_item.due_at,
+    )
+
+
+@router.put("/{todo_item_id}", response_model=ResponseTodoItem)
+def put_todo_item(
+    todo_list_id: int,
+    todo_item_id: int,
+    todo_item: UpdateTodoItem,
+    db: Session = Depends(get_db),
+):
+    existing_item = item_crud.put_todo_item(
+        db,
+        todo_list_id,
+        todo_item_id,
+        todo_item.title,
+        todo_item.description,
+        todo_item.due_at,
+        todo_item.complete,
+    )
+    if not existing_item:
+        raise HTTPException(status_code=404, detail="Todo Item not found")
+    return existing_item
+
+
+@router.delete("/{todo_item_id}")
+def delete_todo_item(
+    todo_list_id: int,
+    todo_item_id: int,
+    db: Session = Depends(get_db),
+):
+    existing_item = item_crud.delete_todo_item(db, todo_list_id, todo_item_id)
+    if not existing_item:
+        raise HTTPException(status_code=404, detail="Todo Item not found")
+    return {}
---
+++ app/routers/list_router.py
@@ -0,0 +1,58 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.crud import list_crud
+from app.dependencies import get_db
+from app.schemas.list_schema import NewTodoList, ResponseTodoList, UpdateTodoList
+
+router = APIRouter(prefix="/lists", tags=["Todoリスト"])
+
+
+@router.get("/{todo_list_id}", response_model=ResponseTodoList)
+def get_todo_list(
+    todo_list_id: int,
+    db: Session = Depends(get_db),
+):
+    todo_list = list_crud.get_todo_list(db, todo_list_id)
+    if not todo_list:
+        raise HTTPException(status_code=404, detail="Todo List not found")
+    return todo_list
+
+
+@router.post("", response_model=ResponseTodoList, status_code=201)
+def post_todo_list(
+    todo_list: NewTodoList,
+    db: Session = Depends(get_db),
+):
+    return list_crud.post_todo_list(db, todo_list.title, todo_list.description)
+
+
+@router.put("/{todo_list_id}", response_model=ResponseTodoList)
+def put_todo_list(
+    todo_list_id: int,
+    todo_list: UpdateTodoList,
+    db: Session = Depends(get_db),
+):
+    if todo_list.title is None:
+        raise HTTPException(status_code=401, detail="Title is required")
+
+    existing_list = list_crud.put_todo_list(
+        db,
+        todo_list_id,
+        todo_list.title,
+        todo_list.description,
+    )
+    if not existing_list:
+        raise HTTPException(status_code=404, detail="Todo List not found")
+    return existing_list
+
+
+@router.delete("/{todo_list_id}")
+def delete_todo_list(
+    todo_list_id: int,
+    db: Session = Depends(get_db),
+):
+    existing_list = list_crud.delete_todo_list(db, todo_list_id)
+    if not existing_list:
+        raise HTTPException(status_code=404, detail="Todo List not found")
+    return {}
